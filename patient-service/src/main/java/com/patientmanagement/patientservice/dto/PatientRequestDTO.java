3cf50eb166dd79d110bfb7ca8b2837c9ba1b71a9
kclim1 <kclim0528@gmail.com>
2025-10-15 19:49:10 +0800

    created a patient request dto

---
+++ patient-service/src/main/java/com/patientmanagement/patientservice/dto/PatientRequestDTO.java
@@ -0,0 +1,16 @@
+package com.patientmanagement.patientservice.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+import jakarta.validation.constraints.Size;
+
+public record PatientRequestDTO(
+        @NotBlank(message = "name is required") @Size(max = 100, message = "name cannot exceed 100 characters") String name,
+        @NotBlank(message = "email should be valid") @Email(message = "email should be valid") String email,
+        @NotBlank(message = "address is required") String address,
+        @NotBlank(message = "date of birth is required") String dateOfBirth,
+        @NotBlank(message = "registered date is required") String registeredDate
+        ) {
+}
+
