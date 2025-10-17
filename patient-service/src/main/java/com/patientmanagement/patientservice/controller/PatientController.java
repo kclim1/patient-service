e69ab3bd1b78765710a1733863ef3795e1774936
kclim1 <kclim0528@gmail.com>
2025-10-17 14:38:30 +0800

    added update and delete patient endpoints

--- patient-service/src/main/java/com/patientmanagement/patientservice/controller/PatientController.java
+++ patient-service/src/main/java/com/patientmanagement/patientservice/controller/PatientController.java
@@ -2,13 +2,16 @@ package com.patientmanagement.patientservice.controller;
 
 import com.patientmanagement.patientservice.dto.PatientRequestDTO;
 import com.patientmanagement.patientservice.dto.PatientResponseDTO;
+import com.patientmanagement.patientservice.dto.validators.CreatePatientValidationGroup;
 import com.patientmanagement.patientservice.service.PatientService;
-import jakarta.validation.Valid;
+import jakarta.validation.groups.Default;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/patients")
@@ -25,9 +28,19 @@ public class PatientController {
         return ResponseEntity.status(HttpStatus.OK).body(patients);
     }
     @PostMapping
-    public ResponseEntity<PatientResponseDTO> createPatient(@Valid @RequestBody PatientRequestDTO patientRequestDTO){
+    public ResponseEntity<PatientResponseDTO> createPatient(@Validated({Default.class , CreatePatientValidationGroup.class}) @RequestBody PatientRequestDTO patientRequestDTO){
         PatientResponseDTO newPatient = patientService.createPatient(patientRequestDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body(newPatient);
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<PatientResponseDTO> updatePatient(@PathVariable UUID id ,@Validated({Default.class }) @RequestBody PatientRequestDTO patientRequestDTO){
+        PatientResponseDTO updatedPatient = patientService.updatePatientById(id , patientRequestDTO);
+        return ResponseEntity.status(HttpStatus.OK).body(updatedPatient);
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletePatient(@PathVariable UUID id ){
+        patientService.deletePatientById(id);
+        return ResponseEntity.noContent().build();
     }
 
 }
