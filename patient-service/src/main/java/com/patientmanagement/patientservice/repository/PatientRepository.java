4cd2b4a8c48053bce94569de8b43c735e04a74f5
kclim1 <kclim0528@gmail.com>
2025-10-17 14:39:07 +0800

    extended repository layer with additional methods

--- patient-service/src/main/java/com/patientmanagement/patientservice/repository/PatientRepository.java
+++ patient-service/src/main/java/com/patientmanagement/patientservice/repository/PatientRepository.java
@@ -3,10 +3,13 @@ package com.patientmanagement.patientservice.repository;
 import com.patientmanagement.patientservice.model.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.UUID;
 
 @Repository
 public interface PatientRepository extends JpaRepository<Patient , UUID> {
+    boolean existsByEmail(String email);
+    boolean existsByEmailAndIdNot(String email , UUID id);
 }
+
+
