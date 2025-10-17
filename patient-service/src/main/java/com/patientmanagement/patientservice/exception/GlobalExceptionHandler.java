10ff559a3d116e989f08c4579ff637751a3ca246
kclim1 <kclim0528@gmail.com>
2025-10-17 14:38:09 +0800

    added custom exception handlers

--- patient-service/src/main/java/com/patientmanagement/patientservice/exception/GlobalExceptionHandler.java
+++ patient-service/src/main/java/com/patientmanagement/patientservice/exception/GlobalExceptionHandler.java
@@ -1,5 +1,8 @@
 package com.patientmanagement.patientservice.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -11,10 +14,27 @@ import java.util.Map;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+    private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<Map<String, String>> handleValidationException(MethodArgumentNotValidException ex) {
         Map<String, String> errors = new HashMap<>();
         ex.getBindingResult().getFieldErrors().forEach(error -> errors.put(error.getField(), error.getDefaultMessage()));
         return ResponseEntity.badRequest().body(errors);
+    }
+
+    @ExceptionHandler(EmailAlreadyExistsException.class)
+    public ResponseEntity<Map<String,String>> handleEmailAlreadyExistsException(EmailAlreadyExistsException ex){
+        log.warn("email address already exists {}" , ex.getMessage());
+        Map<String , String > errors = new HashMap<>();
+        errors.put("message" , ex.getMessage());
+        return ResponseEntity.badRequest().body(errors);
+    }
+    @ExceptionHandler(PatientNotFoundException.class)
+    public ResponseEntity<Map<String, String>> handlePatientNotFoundException(PatientNotFoundException ex){
+        log.warn("patient not found {}" , ex.getMessage());
+        Map<String, String> errors = new HashMap<>();
+        errors.put("message" , ex.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errors);
     }
 }
